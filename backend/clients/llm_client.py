19028119ba18e427ffe911099810e643b06367a0
Taidgh-Robinson <taidgh.robinson@gmail.com>
2026-09-18 13:01:13 -0400

    Add logger (not used yet) + LLM client to query both local and cloud LLMs

---
+++ backend/clients/llm_client.py
@@ -0,0 +1,25 @@
+from litellm import acompletion
+from config.llm_config import LLMConfig
+
+
+class LLMClient():
+    def __init__(self):
+        self.config: LLMConfig = LLMConfig() 
+
+    async def query_llm(self, query: str):
+        if self.config.local_base:
+            response = await acompletion(
+                model=self.config.model,
+                messages=[{"role": "user", "content": query}],
+                api_base=self.config.local_base
+            )
+        else: 
+            response = await acompletion(
+                model=self.config.model,
+                messages=[{"role": "user", "content": query}],
+            )
+
+        return response
+
+         
+
---
+++ backend/config/llm_config.py
@@ -0,0 +1,11 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+from config.env_file_config import ENV_FILE
+
+
+class LLMConfig(BaseSettings):
+    model: str
+    local_base: str = None 
+
+    model_config = SettingsConfigDict(
+        env_file=ENV_FILE, env_prefix="LLM_", extra="ignore"
+    )
