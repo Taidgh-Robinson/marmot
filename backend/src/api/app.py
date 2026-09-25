acaa02c450759e2a6518c935d3378a46c6ab5878
Taidgh-Robinson <taidgh.robinson@gmail.com>
2026-09-25 15:15:46 -0400

    Okay this ain't gonna work forever but now the endpoint returns something

--- backend/src/api/app.py
+++ backend/src/api/app.py
@@ -1,8 +1,17 @@
 #Temp file so I can still use main to test logic, will be moved up to main later
+import httpx
 from fastapi import FastAPI
 from src.api.routers.quote_router import router as quote_router 
+from contextlib import asynccontextmanager
 
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    app.state.http_client = httpx.AsyncClient()
 
-app = FastAPI()
+    yield
+
+    await app.state.http_client.aclose()
+
+app = FastAPI(lifespan=lifespan)
 
 app.include_router(quote_router)
